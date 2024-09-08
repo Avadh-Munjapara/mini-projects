26436e5120e31d434b7689b833d9dc1360d99072
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-09-08 13:17:56 +0530

    current city and flag api added

--- weather/script.js
+++ weather/script.js
@@ -1,9 +1,13 @@
-let permissiontab=document.querySelector("[data-permissiontab]")
-let yweathertab=document.querySelector("[data-yweathertab]")
-let searchtab=document.querySelector("[data-searchtab]")
-let loading=document.querySelector("[data-loading]")
+let permissiontab=document.querySelector("[data-permissiontab]");
+let yweathertab=document.querySelector("[data-yweathertab]");
+let searchtab=document.querySelector("[data-searchtab]");
+let loading=document.querySelector("[data-loading]");
+let city_text=document.querySelector("[data-city]");
+let country_flag=document.querySelector("[data-country]");
 let currtab=permissiontab;
 let cords;
+let city;
+let apikey='02dc0eac1830db04da64d6ce795cdb63';
 function unset_tab(tab){
     tab.style.display="none";
 }
@@ -29,11 +33,30 @@ function switchtab(tab){
 function grant_access(){
      navigator.geolocation.getCurrentPosition((position)=>{
        cords={
-        lett:position.coords.latitude,
+        letti:position.coords.latitude,
         longi:position.coords.longitude,
        };
+    //    sessionStorage.setItem("latti",position.coords.latitude);
+    //    sessionStorage.setItem("longi",position.coords.longitude);
         unset_tab(permissiontab);
         currtab=yweathertab;
         set_tab(yweathertab);
+       set_values();
     })
+}
+
+async function get_info(){
+    let response= await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${cords.letti}&lon=${cords.longi}&appid=${apikey}`);
+    let result=await response.json();
+    let obj={
+        city:result[0].name,
+        country_name:result[0].country,
+    }
+    return obj;
+}
+
+async function set_values(){
+    let info=await get_info();
+    city_text.textContent=info.city;
+    country_flag.setAttribute("src",`https://flagsapi.com/${info.country_name}/flat/24.png`);
 }
