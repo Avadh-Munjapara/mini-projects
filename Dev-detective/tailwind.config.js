972ce216980a4a54a9290aa55f85e933e92119dd
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-09-13 18:18:38 +0530

    html completed

--- Dev-detective/tailwind.config.js
+++ Dev-detective/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        spacemono: ["Space Mono", "monospace"]
+      }
+    },
   },
   plugins: [],
 }
