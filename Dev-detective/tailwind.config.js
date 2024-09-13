b3748acaf6393439049f44e8db10bc05c47e1825
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-09-13 18:55:21 +0530

    heading and search box css added

--- Dev-detective/tailwind.config.js
+++ Dev-detective/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      colors:{
+        backcolor:"#F6F8FF",
+        headcolor:"#4B6A9B",
+        hoverbuttoncolor:"#60ABFF",
+        fontcolor:"#778FB3"
+
+      },
       fontFamily:{
         spacemono: ["Space Mono", "monospace"]
       }
