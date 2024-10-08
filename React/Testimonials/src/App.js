31ab8efcf5bc5951f7dfc0463d733a314e77080e
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-10-08 17:09:54 +0530

    all components created

---
+++ React/Testimonials/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Testimonials from "./components/Testimonials";
+function App() {
+  return (
+    <div>
+      <h1>Our Testimonials</h1>
+      <div></div>
+      <Testimonials/>
+    </div>
+  );
+}
+
+export default App;
