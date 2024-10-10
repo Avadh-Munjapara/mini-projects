a605e521b7f452609d34c87cd92f9ba9df5bd30b
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-10-10 13:38:21 +0530

    navbar completed

--- React/studynotion/src/App.js
+++ React/studynotion/src/App.js
@@ -1,9 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import { Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import { useState } from 'react';
 
 function App() {
+
+  const [IsLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-   <div className='bg-black text-white'>hello ji kese ho sare?</div>
+
+
+    <div className='bg-[#000814]'>
+      <Navbar IsLoggedIn={IsLoggedIn}></Navbar>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+      </Routes>
+    </div>
+
+
+
+
   );
 }
 
--- React/studynotion/src/components/Navbar.js
+++ React/studynotion/src/components/Navbar.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import logo from "../assets/Logo.svg"
 
-const Navbar = () => {
+const Navbar = ({IsLoggedIn}) => {
     return (
-        <div>
-            
+        <div className='flex py-3 justify-evenly text-[#AFB2BF] items-center'>
+            <div><img className='h-9' src={logo} alt="logo" /></div>
+            <div className='flex gap-5'>
+                <button>Home</button>   
+                <button>About</button>
+                <button>Contact</button>
+            </div>
+            {
+                IsLoggedIn?(<div className='flex gap-5'>
+                    <button className='bg-[#161D29] px-5 py-2 rounded-md border-[0.5px] border-[#2C333F]'>Logout</button>
+                    <button className='bg-[#161D29] px-5 py-2 rounded-md border-[0.5px] border-[#2C333F]'>Dashboard</button>
+               </div>):(  <div className='flex gap-5'>
+                <button className='bg-[#161D29] px-5 py-2 rounded-md border-[0.5px] border-[#2C333F]'>Login</button>
+                <button className='bg-[#161D29] px-5 py-2 rounded-md border-[0.5px] border-[#2C333F]'>Signup</button>
+                
+            </div>)
+            }
+          
         </div>
     );
 }
