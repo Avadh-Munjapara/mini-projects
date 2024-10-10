2457ddf9b8236dc8c379285561ce142b28245a72
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-10-10 12:49:51 +0530

    files initialized

---
+++ React/studynotion/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className='bg-black text-white'>hello ji kese ho sare?</div>
+  );
+}
+
+export default App;
