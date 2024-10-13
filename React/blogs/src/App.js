1722ed1554691d2367a8cfb7b88a647e03587084
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-10-13 17:21:40 +0530

    header completed

--- React/blogs/src/App.js
+++ React/blogs/src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pagination from "./components/Pagination";
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Blogs/>
+      <Pagination/>
     </div>
   );  
 }
---
+++ React/blogs/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-center font-bold py-4 border-b-4 shadow-[1px_0px_1px]'>Blogs</h1>
+        </div>
+    );
+}
+
+export default Header;
